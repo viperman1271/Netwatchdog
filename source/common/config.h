ee3878c10cfb15907f1be0cc398e452ae6ebd2d3
Michael Filion <viperman1271@hotmail.com>
2024-05-19 13:33:03 -0400

    Updating config/options

--- source/common/config.h
+++ source/common/config.h
@@ -12,32 +12,85 @@
 
 namespace Config
 {
+    void ConfigureIfEnvVarNotEmpty(toml::value& config, const std::string& category, const std::string& variable, const std::string& envVariable)
+    {
+#ifdef _WIN32
+        constexpr int envVarStrLen = 256;
+        char* value = reinterpret_cast<char*>(alloca(envVarStrLen * sizeof(char)));
+
+        size_t envVarSize = 0;
+        _dupenv_s(&value, &envVarSize, envVariable.c_str());
+        if (envVarSize > 0)
+        {
+            config[category][variable] = value;
+        }
+#else
+        char* value = getenv(envVariable.c_str());
+        if (value != nullptr && strlen(value) > 0)
+        {
+            config[category][variable] = value;
+        }
+#endif
+    }
+
+    template<typename T>
+    void ConfigureDefaultValue(toml::value& config, const std::string& category, const std::string& variable, const T& defaultValue) noexcept
+    {
+        auto& tab = config.as_table();
+        if (tab.count(category) == 0)
+        {
+            config[category][variable] = defaultValue;
+        }
+        else
+        {
+            auto& subtab = config[category].as_table();
+            if (subtab.count(variable) == 0)
+            {
+                config[category][variable] = defaultValue;
+            }
+        }
+    }
+
     void LoadOrCreateConfig(Options& options)
     {
         const std::filesystem::path& configPath = Utils::GetConfigPath();
+        const bool configFileExists = std::filesystem::exists(configPath);
 
         toml::value config;
-        if (!std::filesystem::exists(configPath))
+        if(configFileExists)
+        {
+            std::cout << "Loading configuration file [" << configPath.string() << "]." << std::endl;
+            config = toml::parse(configPath.string());
+        }
+
+        ConfigureDefaultValue(config, "server", "host", "*");
+        ConfigureDefaultValue(config, "server", "port", options.port);
+        ConfigureDefaultValue(config, "server", "identity", uuids::to_string(uuids::uuid_random_generator(g_RNG)()));
+
+        ConfigureDefaultValue(config, "client", "host", "localhost");
+        ConfigureDefaultValue(config, "client", "port", options.port);
+        ConfigureDefaultValue(config, "client", "identity", uuids::to_string(uuids::uuid_random_generator(g_RNG)()));
+
+        ConfigureDefaultValue(config, "database", "username", "root");
+        ConfigureDefaultValue(config, "database", "password", "password1234");
+        ConfigureDefaultValue(config, "database", "host", "localhost");
+        ConfigureDefaultValue(config, "database", "port", 27017);
+
+        if (!configFileExists)
         {
             std::cout << "Configuration file [" << configPath.string() << "] not found... creating." << std::endl;
-
-            config["server"]["host"] = "*";
-            config["server"]["port"] = options.port;
-            config["server"]["identity"] = uuids::to_string(uuids::uuid_random_generator(g_RNG)());
-
-            config["client"]["host"] = "localhost";
-            config["client"]["port"] = options.port;
-            config["client"]["identity"] = uuids::to_string(uuids::uuid_random_generator(g_RNG)());
 
             std::ofstream ofs(configPath.string());
             ofs << config;
             ofs.close();
         }
-        else
-        {
-            std::cout << "Loading configuration file [" << configPath.string() << "]." << std::endl;
-            config = toml::parse(configPath.string());
-        }
+
+#ifdef NETWATCHDOG_SERVER
+        ConfigureIfEnvVarNotEmpty(config, "database", "username", "MONGO_USERNAME");
+        ConfigureIfEnvVarNotEmpty(config, "database", "password", "MONGO_PASSWORD");
+        ConfigureIfEnvVarNotEmpty(config, "database", "host", "MONGO_HOST");
+        ConfigureIfEnvVarNotEmpty(config, "database", "port", "MONGO_PORT");
+#endif // NETWATCHDOG_SERVER
 
 #ifdef NETWATCHDOG_CLIENT
         constexpr const char* CATEGORY = "client";
@@ -48,6 +101,13 @@ namespace Config
         options.host = toml::find<std::string>(config, CATEGORY, "host");
         options.port = toml::find<int>(config, CATEGORY, "port");
         options.identity = toml::find<std::string>(config, CATEGORY, "identity");
+
+#ifdef NETWATCHDOG_SERVER
+        options.database.username = toml::find<std::string>(config, "database", "username");
+        options.database.password = toml::find<std::string>(config, "database", "password");
+        options.database.host = toml::find<std::string>(config, "database", "host");
+        options.database.port = toml::find<int>(config, "database", "port");
+#endif // NETWATCHDOG_SERVER
     }
 
     bool ParseCommandLineOptions(int argc, char** argv, Options& options)
@@ -61,6 +121,11 @@ namespace Config
         app.add_option("--host", options.host, "Host to connect to");
 #elif defined(NETWATCHDOG_SERVER)
         app.add_option("--host", options.host, "Listening address for the server [defaults to *]");
+
+        app.add_option("--username", options.database.username, "Username for database access [defaults to root]");
+        app.add_option("--password", options.database.password, "Password for database access");
+        app.add_option("--db_host", options.database.host, "Database host address");
+        app.add_option("--db_port", options.database.port, "Database port [defaults to 27017]");
 #endif // NETWATCHDOG_CLIENT / NETWATCHDOG_SERVER
 
         try

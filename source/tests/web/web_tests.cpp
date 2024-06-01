e18b1bdbdc84d89a0bc8e828b9257768f80e5d35
Michael Filion <viperman1271@hotmail.com>
2024-06-01 17:05:56 -0400

    Setting the certificate, public key, & private key for the generation tests.

--- source/tests/web/web_tests.cpp
+++ source/tests/web/web_tests.cpp
@@ -11,6 +11,14 @@
 #include <windows.h>
 #endif // _WIN32
 
+void DeleteIfExists(const std::filesystem::path& path)
+{
+    if (std::filesystem::exists(path))
+    {
+        std::filesystem::remove(path);
+    }
+}
+
 TEST_CASE("Ensure that OPENSSL_Applink is available")
 {
 #ifdef _WIN32
@@ -26,6 +34,23 @@ TEST_CASE("Private/Public key + certificate generated as expected")
 {
     Options options;
     Config::LoadOrCreateConfig(options);
+
+    std::filesystem::path certificatePath = std::filesystem::current_path();
+    certificatePath /= "certificate.pem";
+
+    std::filesystem::path publicKeyPath = std::filesystem::current_path();
+    publicKeyPath /= "public_key.pem";
+
+    std::filesystem::path privateKeyPath = std::filesystem::current_path();
+    privateKeyPath /= "private_key.pem";
+
+    options.web.certificatePath = certificatePath.string();
+    options.web.publicKeyPath = publicKeyPath.string();
+    options.web.privateKeyPath = privateKeyPath.string();
+
+    DeleteIfExists(certificatePath);
+    DeleteIfExists(publicKeyPath);
+    DeleteIfExists(privateKeyPath);
 
     WebServer webServer(options);
     options = webServer.GetOptions();

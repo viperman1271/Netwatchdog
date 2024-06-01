9d2155bb259d6365bae521df57c9014aa8c35fbe
Michael Filion <viperman1271@hotmail.com>
2024-06-01 17:04:22 -0400

    Creating new catch session to enable test mode for web tests

--- source/tests/web/main.cpp
+++ source/tests/web/main.cpp
@@ -1,3 +1,27 @@
 #define CATCH_CONFIG_MAIN
 
+#include "tests.h"
+
 #include <catch.hpp>
+
+class DefaultSession : public Catch::Session 
+{
+public:
+    DefaultSession() 
+    {
+        Tests::SetIsTestMode(true);
+    }
+};
+
+int main(int argc, char* argv[]) 
+{
+    // Use the custom session to initialize the variable
+    DefaultSession session;
+    int result = session.applyCommandLine(argc, argv);
+    if (result != 0) // Indicates a command line error
+    {
+        return result;
+    }
+    
+    return session.run();
+}
